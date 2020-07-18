b547c06ea1555fbd053247611e6a7b8262e6f960
Kirill Skvortsov <truemancheg@gmail.com>
2020-07-18 18:08:42 +0300

    add overtime popuping block

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -14,6 +14,21 @@ export default class Slider {
             this.slideIndex = this.slides.length;
         }
 
+        try {
+            this.hanson.style.opacity = "0";
+
+            if (n === 3) {
+                this.hanson.classList.add("animated");
+                setTimeout(() => {
+                    this.hanson.style.opacity = 1;
+                    this.hanson.classList.add("slideInUp");
+                }, 3000);
+            } else {
+                this.hanson.classList.remove("slideInUp");
+            }
+        } catch(e) {}
+        
+
         this.slides.forEach(slide => {
             slide.style.display = "none";
         });
@@ -26,6 +41,12 @@ export default class Slider {
     }
 
     render() {
+        try {
+            this.hanson = document.querySelector(".hanson");
+        } catch(e) {
+            console.log(e);
+        }
+        
         this.btns.forEach(btn => {
             btn.addEventListener("click", () => this.plusSlides(1));
             btn.parentNode.previousElementSibling.addEventListener("click", (e) =>{
