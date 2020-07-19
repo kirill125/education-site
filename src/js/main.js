016584c0e264f7a5f326afd7ed2cdbdc29910df6
Kirill Skvortsov <truemancheg@gmail.com>
2020-07-19 15:44:34 +0300

    add block with differences

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import MainSlider from "./modules/slider/slider-main";
 import MiniSlider from "./modules/slider/slider-mini";
 import VideoPlayer from "./modules/playVideo";
+import Difference from "./modules/difference";
 
 window.addEventListener("DOMContentLoaded", () => {
     const slider = new MainSlider({container: ".page", btns: ".next"});
@@ -36,4 +37,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const videoPlayer = new VideoPlayer(".showup .play", ".overlay");
     videoPlayer.init(".play", ".overlay");
+
+    new Difference(".officerold", ".officernew", ".officer__card-item").init();
 });
